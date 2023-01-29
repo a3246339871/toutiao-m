db7a183731e3b5b3deda3ea89bb3eb3a773f5818
秋安 <12023846+makabaka0118@user.noreply.gitee.com>
2023-01-29 08:54:23 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    open: true,
+    host: 'localhost'
+
+  },
+  // 关闭eslint
+  lintOnSave: false
 })
